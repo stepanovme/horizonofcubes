56ac790185331d3fdbf163b50eb135799cd85d2e
Степан Степанов <st.stepanov57@gmail.com>
2023-10-11 09:29:12 +0300

    Initial commit

---
+++ header.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/bootstrap-grid.min.css">
+    <script type="text/javascript" src="./js/script.js"></script>
+</head>
+<body>
+
+    <header>
+        <div class="navbar">
+            <div class="logo"><a href="#">Logo</a></div>
+
+            <ul class="links">
+                <li><a href="">Главная</a></li>
+                <li><a href="">Начать играть</a></li>
+                <li><a href="">Правила</a></li>
+                <li><a href="">Вики</a></li>
+                <li><a href="">О проекте</a></li>
+                <li><a href="">Прочее</a></li>
+            </ul>
+            <a href="#" class="action_btn">Вход</a>
+        </div>
+    </header>
+
+</body>
+</html>
