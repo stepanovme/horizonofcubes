a9bd2f863fb5d2d2c11758d99bd2e34a8ef32af8
Astafiev Stepanov <astafiev@stepanov.com>
2023-10-18 15:40:22 +0300

    Add header

---
+++ head.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="./css/main.css">
+<link rel="stylesheet" href="./css/bootstrap-grid.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="icon" href="../assets/images/favicon/favicon.ico">
+<script type="text/javascript" src="./js/script.js"></script>
--- index.php
+++ index.php
@@ -1,40 +1,13 @@
 <!doctype html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./css/main.css">
-    <link rel="stylesheet" href="./css/bootstrap-grid.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script type="text/javascript" src="./js/script.js"></script>
+<?php include 'head.php'; ?>
+    <link rel="icon" href="favicon.ico">
     <title>Horizon Of Cubes - Главная</title>
 </head>
 <body>
 
-<header>
-    <div class="navbar">
-        <div class="logo">
-            <a href="#">Logo</a>
-        </div>
-
-        <ul class="links">
-            <li><a href="">Главная</a></li>
-            <li><a href="">Начать играть</a></li>
-            <li><a href="">Правила</a></li>
-            <li><a href="">Вики</a></li>
-            <li><a href="">О проекте</a></li>
-            <li><a href="">Прочее</a></li>
-        </ul>
-
-        <a href="#" class="action-btn">Вход</a>
-        <div class="toggle_btn"><i class="fa-solid fa-bars"></i></div>
-    </div>
-</header>
+    <?php include 'header.php'; ?>
 
 </body>
 </html>
