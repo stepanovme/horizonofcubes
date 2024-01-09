88161305b5a64f7e1641627552b3b71224a4111b
Stepan Stepanov <st.stepanov57@gmail.com>
2024-01-09 13:07:58 +0300

    mobile header v0.1

--- head.php
+++ head.php
@@ -9,4 +9,5 @@
 <link rel="stylesheet" href="./css/bootstrap-grid.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="icon" href="./assets/images/favicon/favicon.ico">
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 <script type="text/javascript" src="./js/script.js"></script>
--- header.php
+++ header.php
@@ -19,6 +19,18 @@
             <a class="light-mode"><i class="fa-regular fa-moon fa-xl" style="color: #ffffff; cursor: pointer;"></i></a>
             <a class="discord-link" href="https://discord.com/invite/AHKee2wd2b" target="_blank"><i class="fa-brands fa-discord fa-xl" style="color: #ffffff;"></i></a>
             <a href="#" class="action_btn">Вход</a>
+            <label for="" class="icons"><i class='bx bx-menu'></i></label>
         </div>
     </div>
 </header>
+
+<div class="subheader">
+    <ul class="links">
+            <li><a href="" class="active">Главная</a></li>
+            <li><a href="">Начать играть</a></li>
+            <li><a href="">Правила</a></li>
+            <li><a href="">Вики</a></li>
+            <li><a href="">О проекте</a></li>
+            <li><a href="">Прочее</a></li>
+    </ul>
+</div>
